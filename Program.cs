c7355d647fcade84c6f5ad333cd0d357bdc377c2
Hijjaj <hijjaj.refza.ft20@mail.umy.ac.id>
2022-03-16 14:12:26 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Koneksi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program().Connecting();
+        }
+
+        public void Connecting()
+        {
+            using (
+                SqlConnection con = new SqlConnection("data source=LAPTOP-OG8P1VHL\\HIJJAJ;database=ProdiTI;Integrated Security=true")
+                )
+            {
+                con.Open();
+                Console.WriteLine("Berhasil terhubung ke database!");
+            }
+        }
+    }
+}
